0d08cb34e2c4022b57d08ba82da45b6289a888f9
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-10 17:04:48 +0100

    eventHandlers

---
+++ script.js
@@ -0,0 +1,11 @@
+const submit = document.getElementById('submit');
+submit.style.backgroundColor = 'red';
+submit.style.padding ='8px 20px'
+submit.style.borderRadius = '10px'
+submit.style.border = '1px solid white'
+ document.body.style.backgroundColor = 'grey';
+ document.body.style.textAlign = 'center';
+ document.body.style.padding = '150px';
+submit.addEventListener('click',()=>{
+    alert('Thank you for banking with us')
+})
